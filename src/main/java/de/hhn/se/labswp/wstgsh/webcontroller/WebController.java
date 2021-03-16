9d217f602d3a4c11f82a1899fc479bcf089b0007
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-03-16 11:14:37 +0100

    Dem WebController eine Instanz der ReisepunktService hinzugefügt

--- src/main/java/de/hhn/se/labswp/wstgsh/webcontroller/WebController.java
+++ src/main/java/de/hhn/se/labswp/wstgsh/webcontroller/WebController.java
@@ -1,11 +1,29 @@
 package de.hhn.se.labswp.wstgsh.webcontroller;
 
-import org.springframework.stereotype.Controller;
+import de.hhn.se.labswp.wstgsh.webapi.models.Reisepunkt;
+import de.hhn.se.labswp.wstgsh.webapi.models.ReisepunktService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path="/")
+import java.util.List;
+
+@RestController
+@RequestMapping(path="find")
 public class WebController {
     private static final org.slf4j.Logger logger =
             org.slf4j.LoggerFactory.getLogger(WebController.class);
+
+    private final ReisepunktService reisepunktService;
+
+    @Autowired
+    public WebController(ReisepunktService reisepunktService) {
+        this.reisepunktService = reisepunktService;
+    }
+
+    @GetMapping
+    public List<Reisepunkt> getReisepuntke() {
+        return reisepunktService.getReisepunkte();
+    }
 }
