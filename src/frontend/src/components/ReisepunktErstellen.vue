c8d84306d4a6df11a21cc2011adabcbffac3721f
Benjamin Kammer <bkammer@stud.hs-heilbronn.de>
2021-03-22 10:01:07 +0100

    Added html form to component

---
+++ src/frontend/src/components/ReisepunktErstellen.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Reisepunkt erstellen</h1>
+    <form>
+      <label for="rpname">Reisepunktname:</label>
+      <input type="text" id="rpname" name="rpname"><br />
+      <label for="rpdescription">Beschreibung:</label>
+      <input type="text" id="rpdescription" name="rpdescription"><br />
+      <h3>Typ</h3>
+      <input type="radio" id="rppunkt" name="rptype" value="punkt">
+      <label for="rppunkt">Attraktion</label><br />
+      <input type="radio" id="rpattraktion" name="rptype" value="attraktion">
+      <label for="rpattraktion">Attraktion</label><br />
+      <input type="radio" id="rpsehenswuerdigkeit" name="rptype" value="sehenswuerdigkeit">
+      <label for="rpsehenswuerdigkeit">Sehenswürdigkeit</label>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReisepunktErstellen"
+}
+</script>
+
+<style scoped>
+
+</style>
