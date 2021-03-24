c369e88c589c2905919cff014b0321a514c61508
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-03-24 16:46:47 +0100

    Created Unit Test classes for existing controller

---
+++ src/test/java/de/hhn/se/labswp/wstgsh/webcontroller/AttraktionControllerTest.java
@@ -0,0 +1,28 @@
+package de.hhn.se.labswp.wstgsh.webcontroller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttraktionControllerTest {
+
+  @Test
+  void all() {
+  }
+
+  @Test
+  void one() {
+  }
+
+  @Test
+  void newAttraktion() {
+  }
+
+  @Test
+  void replaceAttraktion() {
+  }
+
+  @Test
+  void deleteAttraktion() {
+  }
+}
