ad39291737b3994718e2f0e5a67af4e090ad07a0
Robert Pistea <rpistea@stud.hs-heilbronn.de>
2021-04-01 13:30:10 +0200

    Leaflet importiert, Map geschrieben

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <Map/>
+  <div>
+    <Map/>
+  </div>
 </template>
 
 <script>
 import Map from './components/Map.vue'
+import {LMap, LMarker, LTileLayer} from "vue2-leaflet";
+//import {LMarker, LMap, LTileLayer} from "vue2-leaflet";
 
 export default {
   name: 'App',
   components: {
-    Map
+    Map,
   }
 }
 </script>
 
 <style>
 #app {
-
+  height: 100%;
+  width: 100%;
 }
 </style>
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,4 +1,16 @@
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 
 createApp(App).mount('#app')
