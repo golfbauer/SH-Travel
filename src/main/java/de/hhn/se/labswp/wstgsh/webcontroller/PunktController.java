9fabc1bc8f2d2974c9cbda09e0475d38db83378f
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-04-08 17:37:52 +0200

    Adjusted Entity and Controller Classes to the new Database

--- src/main/java/de/hhn/se/labswp/wstgsh/webcontroller/PunktController.java
+++ src/main/java/de/hhn/se/labswp/wstgsh/webcontroller/PunktController.java
@@ -1,10 +1,7 @@
 package de.hhn.se.labswp.wstgsh.webcontroller;
 
-import de.hhn.se.labswp.wstgsh.webapi.models.Attraktion;
-import de.hhn.se.labswp.wstgsh.webapi.models.AttraktionRepository;
 import de.hhn.se.labswp.wstgsh.webapi.models.Punkt;
 import de.hhn.se.labswp.wstgsh.webapi.models.PunktRepository;
-import de.hhn.se.labswp.wstgsh.webapi.models.Reisepunkt;
 import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PunktController {
+
   private final PunktRepository repository;
+
   PunktController(PunktRepository repository) {
     this.repository = repository;
   }
