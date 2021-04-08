d13af710a2ab61d791f1d0b38c6677984930fbfb
Benjamin Kammer <bkammer@stud.hs-heilbronn.de>
2021-04-08 18:33:42 +0200

    toggeld Sidbar with Burger and Close button

--- src/frontend/src/components/SidebarMenu.vue
+++ src/frontend/src/components/SidebarMenu.vue
@@ -1,37 +1,48 @@
 <template>
-  <div id="menu" :class="{ active: isExpanded }" @click.prevent="toggle">
-    <div class="grid-container menu-header" v-if="profile.signedIn">
-      <img :src="profile.img" id="profile-img" :alt="profile.name"/>
-      <div id="profile-name">{{ profile.name }}</div>
-      <div id="profile-typ">{{ profile.typ }}</div>
+  <div>
+    <!-- Burger Button -->
+    <div id="burger">
+      <b-button v-b-toggle.sidebar-no-header type="button" class="burger-button" title="Menu">
+        <span class="burger-bar burger-bar--1"></span>
+        <span class="burger-bar burger-bar--2"></span>
+        <span class="burger-bar burger-bar--3"></span>
+      </b-button>
     </div>
-    <div class="menu-header" v-else>
-      <button class="btn btn-orange">Registrieren</button>
-      <button class="btn btn-gray">Anmelden</button>
-    </div>
-    <div id="menu-items">
-      <!--sidebar-menu-item
-          v-for="(item, index) in menu"
-          :key="index"
-          :item="item"
-      /-->
-    </div>
+    <!-- Sidebar -->
+    <b-sidebar id="sidebar-no-header" aria-labelledby="sidebar-no-header-title" no-header shadow>
+      <div class="grid-container menu-header" v-if="profile.signedIn">
+        <img :src="profile.img" id="profile-img"/>
+        <div id="profile-name">{{ profile.name }}</div>
+        <div id="profile-typ">{{ profile.typ }}</div>
+      </div>
+      <div class="menu-header" v-else>
+        <button class="btn btn-orange">Registrieren</button>
+        <button class="btn btn-gray">Anmelden</button>
+      </div>
+      <!-- SidebarMenuItems -->
+      <div id="menu-items">
+        <!--sidebar-menu-item
+            v-for="(item, index) in menu"
+            :key="index"
+            :item="item"
+        /-->
+      </div>
+      <!-- End SidebarMenuItems -->
+      <b-button v-b-toggle.sidebar-no-header id="btn-close" type="button" title="Menu">
+        &lt;&lt;
+      </b-button>
+    </b-sidebar>
+    <!-- End Sidebar -->
   </div>
 </template>
 
 <script>
-// import SidebarMenuItem from './SidebarMenuItem.vue'
+// import SidebarMenuItem from '@/components/SidebarMenuItem'
 
 export default {
   name: 'Menu',
   components: {
     // SidebarMenuItem
-  },
-  props: {
-    menu: {
-      type: Array,
-      required: true
-    }
   },
   data () {
     return {
@@ -46,9 +57,6 @@ export default {
     }
   },
   methods: {
-    toggle () {
-      this.isExpanded = !this.isExpanded
-    },
     get: function () {
       this.$http.get('')
     }
@@ -57,6 +65,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+$background-color-1: rgb(0, 124, 83);
+$background-color-2: rgb(0, 172, 114);
+$orange: rgb(194, 126, 0);
+
 #menu {
   position: fixed;
   top: 0;
@@ -65,11 +78,11 @@ export default {
   width: 16rem;
   max-width: 100%;
   margin: 0;
-  background-color: rgb(0, 172, 114);
+  background-color: $background-color-2;
 }
 
 .menu-header {
-  background-color: rgb(0, 124, 83);
+  background-color: $background-color-1;
 }
 
 .grid-container {
@@ -99,15 +112,83 @@ export default {
   width: 7rem;
   border-radius: 0.3rem;
   border: 0;
+
+  &-gray {
+    background-color: gray;
+    color: #fff;
+  }
+
+  &-orange {
+    background-color: $orange;
+    color: #fff;
+  }
 }
 
-.btn-gray {
-  background-color: gray;
-  color: #fff;
+#btn-close {
+  position: absolute;
+  display: block;
+  width: 2.5rem;
+  background-color: $orange;
+  border-radius: 50%;
+  bottom: 0;
+  right: 0;
 }
 
-.btn-orange {
-  background-color: orange;
-  color: #fff;
+// Burger Icon
+button {
+  cursor: pointer;
+  /* remove blue outline */
+  &:focus {
+    outline: 0;
+  }
 }
+
+.burger-button {
+  position: absolute;
+  top: 7px;
+  left: 7px;
+  height: 30px;
+  width: 32px;
+  display: block;
+  z-index: 999;
+  border: 0;
+  border-radius: 0;
+  background-color: transparent;
+  pointer-events: all;
+  transition: transform .6s cubic-bezier(.165, .84, .44, 1);
+}
+
+.burger-bar {
+  background-color: #130f40;
+  position: absolute;
+  top: 50%;
+  right: 6px;
+  left: 6px;
+  height: 2px;
+  width: auto;
+  transition: transform .6s cubic-bezier(.165, .84, .44, 1), opacity .3s cubic-bezier(.165, .84, .44, 1), background-color .6s cubic-bezier(.165, .84, .44, 1);
+}
+
+.burger-bar--1 {
+  -webkit-transform: translateY(-6px);
+  transform: translateY(-6px);
+}
+
+.burger-bar--2 {
+  transform-origin: 100% 50%;
+  transform: scaleX(.8);
+}
+
+.burger-button:hover .burger-bar--2 {
+  transform: scaleX(1);
+}
+
+.no-touchevents .burger-bar--2:hover {
+  transform: scaleX(1);
+}
+
+.burger-bar--3 {
+  transform: translateY(6px);
+}
+// End Burger Icon
 </style>
