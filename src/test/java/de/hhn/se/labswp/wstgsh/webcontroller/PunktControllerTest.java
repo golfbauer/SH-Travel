fd1007a657013594e8c55866d82ee7dc45657513
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-04-14 18:51:36 +0200

    So brian wont have a merge conflict

--- src/test/java/de/hhn/se/labswp/wstgsh/webcontroller/PunktControllerTest.java
+++ src/test/java/de/hhn/se/labswp/wstgsh/webcontroller/PunktControllerTest.java
@@ -1,17 +1,60 @@
 package de.hhn.se.labswp.wstgsh.webcontroller;
 
+import de.hhn.se.labswp.wstgsh.webapi.models.Punkt;
+import de.hhn.se.labswp.wstgsh.webapi.models.PunktRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class PunktControllerTest {
 
-  @Test
-  void all() {
+  @Mock
+  private PunktRepository punktRepository;
+  private PunktController underTest;
+
+  @BeforeEach
+  void setUp() {
+    underTest = new PunktController(punktRepository);
   }
 
   @Test
-  void one() {
+  void canGetAllPunkte() {
+    //when
+    underTest.all();
+    //then
+    verify(punktRepository).findAll();
+  }
+
+  @Test
+  void canGetPunktWithId() {
+    //given
+    long id  = 10;
+    given(punktRepository.findById(id)).willReturn(java.util.Optional.of(new Punkt()));
+    //when
+    underTest.one(id);
+    //then
+    verify(punktRepository).findById(id);
+  }
+
+  @Test
+  void punktWithIdDoesntExist() {
+    //given
+    long id = 10;
+    given(punktRepository.findById(id)).willReturn(Optional.empty());
+    //when
+    //then
+    assertThatThrownBy(() -> underTest.one(id))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessageContaining("id not found");
   }
 
   @Test
