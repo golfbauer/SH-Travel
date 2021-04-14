a7d0b54a114b7e207201d051df3c2b7306f7d161
Benjamin Kammer <bkammer@stud.hs-heilbronn.de>
2021-04-14 19:16:19 +0200

    Implemented OneToMany and ManyToOne relation

--- src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/Attraktion.java
+++ src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/Attraktion.java
@@ -2,13 +2,8 @@ package de.hhn.se.labswp.wstgsh.webapi.models;
 
 import de.hhn.se.labswp.wstgsh.exceptions.ReisepunktNotFoundAdvice;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
 
 @Entity(name = "attraktion")
 @Table(name = "attraktion")
@@ -22,20 +17,24 @@ public class Attraktion extends Reisepunkt {
   @Column(name = "Beschreibung")
   private String beschreibung;
 
+  @OneToMany(mappedBy = "attraktion")
+  private ArrayList<AttraktionOeffnungszeit> attraktionOeffnungszeiten;
+
   /**
    * Constructor to create an object, which can be implemented into the database if needed.
-   * @param id Inherited by Reisepunkt.
-   * @param laengengrad Marks exact locaion of Attraktion, North to South.
-   * @param breitengrad Marks exact locaion of Attraktion, West to East.
-   * @param nutzerEmail Email of the creator account.
-   * @param name Name of the created Attraktion.
+   *
+   * @param id           Inherited by Reisepunkt.
+   * @param laengengrad  Marks exact locaion of Attraktion, North to South.
+   * @param breitengrad  Marks exact locaion of Attraktion, West to East.
+   * @param nutzerEmail  Email of the creator account.
+   * @param name         Name of the created Attraktion.
    * @param beschreibung Short description, which gives a explenation of the Attraktion.
-
    */
   public Attraktion(Long id, Float laengengrad, Float breitengrad, String nutzerEmail, String name,
-                    String beschreibung) {
+                    String beschreibung, ArrayList<AttraktionOeffnungszeit> attraktionOeffnungszeiten) {
     super(id, laengengrad, breitengrad, nutzerEmail, name);
     this.beschreibung = beschreibung;
+    this.attraktionOeffnungszeiten = attraktionOeffnungszeiten;
   }
 
   public Attraktion() {
@@ -46,7 +45,15 @@ public class Attraktion extends Reisepunkt {
     return beschreibung;
   }
 
+  public ArrayList<AttraktionOeffnungszeit> getAttraktionOeffnungszeiten() {
+    return attraktionOeffnungszeiten;
+  }
+
   public void setBeschreibung(String beschreibung) {
     this.beschreibung = beschreibung;
+  }
+
+  public void setAttraktionOeffnungszeiten(ArrayList<AttraktionOeffnungszeit> attraktionOeffnungszeiten) {
+    this.attraktionOeffnungszeiten = attraktionOeffnungszeiten;
   }
 }
--- src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/AttraktionOeffnungszeit.java
+++ src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/AttraktionOeffnungszeit.java
@@ -17,24 +17,32 @@ public class AttraktionOeffnungszeit {
   @Column(name = "Oeffnungszeit")
   private String oeffnungszeit;
 
+  @ManyToOne
+  @JoinColumn(name = "AttraktionReisepunkteID", nullable = false)
+  private Attraktion attraktion;
+
   /**
    * Constructor to create an object, which can be implemented into the database if needed.
    *
    * @param id            Id to be created for unique Primary Key.
    * @param oeffnungszeit Opening time for the Attraktion
+   * @param attraktion    Attraktion which this opening time is linked
    */
-  public AttraktionOeffnungszeit(int id, String oeffnungszeit) {
+  public AttraktionOeffnungszeit(int id, String oeffnungszeit, Attraktion attraktion) {
     this.id = id;
     this.oeffnungszeit = oeffnungszeit;
+    this.attraktion = attraktion;
   }
 
   /**
    * Constructor to create an object, with an automaticly generated Primary Key.
    *
    * @param oeffnungszeit Opening time for the Attraktion
+   * @param attraktion    Attraktion which this opening time is linked
    */
-  public AttraktionOeffnungszeit(String oeffnungszeit) {
+  public AttraktionOeffnungszeit(String oeffnungszeit, Attraktion attraktion) {
     this.oeffnungszeit = oeffnungszeit;
+    this.attraktion = attraktion;
   }
 
   public AttraktionOeffnungszeit() {
@@ -45,6 +53,10 @@ public class AttraktionOeffnungszeit {
     this.oeffnungszeit = oeffnungszeit;
   }
 
+  public void setAttraktion(Attraktion attraktion) {
+    this.attraktion = attraktion;
+  }
+
   public int getId() {
     return id;
   }
@@ -53,11 +65,16 @@ public class AttraktionOeffnungszeit {
     return oeffnungszeit;
   }
 
+  public Attraktion getAttraktion() {
+    return attraktion;
+  }
+
   @Override
   public String toString() {
     return "AttraktionOeffnungszeit{"
             + "id=" + id
             + ", oeffnungszeit=" + oeffnungszeit
+            + ", attraktion=" + attraktion
             + '}';
   }
 }
