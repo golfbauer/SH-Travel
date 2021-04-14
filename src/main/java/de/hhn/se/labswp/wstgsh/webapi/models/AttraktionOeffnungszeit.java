2a1710232335505e351fa720aa8c238ddcbc9a47
Benjamin Kammer <bkammer@stud.hs-heilbronn.de>
2021-04-14 18:28:02 +0200

    Formatting

--- src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/AttraktionOeffnungszeit.java
+++ src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/AttraktionOeffnungszeit.java
@@ -7,75 +7,63 @@ import javax.persistence.*;
 
 @Entity(name = "attraktion_oeffungszeit")
 @Table(name = "attraktion_oeffungszeit")
-@PrimaryKeyJoinColumn(name = "AttraktionIndex", referencedColumnName = "ReisepunkteID")
 
 public class AttraktionOeffnungszeit {
-    private static final org.slf4j.Logger logger =
-            org.slf4j.LoggerFactory.getLogger(ReisepunktNotFoundAdvice.class);
-    @Id
-    @Column(name = "ID", updatable = false)
-    private int id;
+  private static final org.slf4j.Logger logger =
+          org.slf4j.LoggerFactory.getLogger(ReisepunktNotFoundAdvice.class);
+  @Id
+  @Column(name = "AttraktionIndex", updatable = false)
+  private int id;
 
-    @Column(name = "Oeffnungszeit")
-    private String oeffnungszeit;
+  @Column(name = "Oeffnungszeit")
+  private String oeffnungszeit;
 
-    @Column(name = "AttraktionReisepunkteID")
-    private int attraktionReisepunkteID;
+  /**
+   * Constructor to create an object, which can be implemented into the database if needed.
+   *
+   * @param id                      Id to be created for unique Primary Key.
+   * @param oeffnungszeit           Opening time for the Attraktion
+   * @param attraktionReisepunkteID Attraktion id which this opening time is linked
+   */
+  public AttraktionOeffnungszeit(int id, String oeffnungszeit, int attraktionReisepunkteID) {
+    this.id = id;
+    this.oeffnungszeit = oeffnungszeit;
+  }
 
-    /**
-     * Constructor to create an object, which can be implemented into the database if needed.
-     *
-     * @param id                      Id to be created for unique Primary Key.
-     * @param oeffnungszeit           Opening time for the Attraktion
-     * @param attraktionReisepunkteID Attraktion id which this opening time is linked
-     */
-    public AttraktionOeffnungszeit(int id, String oeffnungszeit, int attraktionReisepunkteID) {
-        this.id = id;
-        this.oeffnungszeit = oeffnungszeit;
-        this.attraktionReisepunkteID = attraktionReisepunkteID;
-    }
+  /**
+   * Constructor to create an object, with an automaticly generated Primary Key.
+   *
+   * @param oeffnungszeit           Opening time for the Attraktion
+   * @param attraktionReisepunkteID Attraktion id which this opening time is linked
+   */
+  public AttraktionOeffnungszeit(String oeffnungszeit, int attraktionReisepunkteID) {
+    this.oeffnungszeit = oeffnungszeit;
+  }
 
-    /**
-     * Constructor to create an object, with an automaticly generated Primary Key.
-     *
-     * @param oeffnungszeit           Opening time for the Attraktion
-     * @param attraktionReisepunkteID Attraktion id which this opening time is linked
-     */
-    public AttraktionOeffnungszeit(String oeffnungszeit, int attraktionReisepunkteID) {
-        this.oeffnungszeit = oeffnungszeit;
-        this.attraktionReisepunkteID = attraktionReisepunkteID;
-    }
+  public AttraktionOeffnungszeit() {
 
-    public AttraktionOeffnungszeit() {
+  }
 
-    }
+  public void setOeffnungszeit(String oeffnungszeit) {
+    this.oeffnungszeit = oeffnungszeit;
+  }
 
-    public void setOeffnungszeit(String oeffnungszeit) {
-        this.oeffnungszeit = oeffnungszeit;
-    }
+  public void setAttraktionReisepunkteID(int attraktionReisepunkteID) {
+  }
 
-    public void setAttraktionReisepunkteID(int attraktionReisepunkteID) {
-        this.attraktionReisepunkteID = attraktionReisepunkteID;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public String getOeffnungszeit() {
+    return oeffnungszeit;
+  }
 
-    public String getOeffnungszeit() {
-        return oeffnungszeit;
-    }
-
-    public int getAttraktionReisepunkteID() {
-        return attraktionReisepunkteID;
-    }
-
-    @Override
-    public String toString() {
-        return "AttraktionOeffnungszeit{"
-                + "id=" + id
-                + ", oeffnungszeit=" + oeffnungszeit
-                + ", attraktionReisepunkteID=" + attraktionReisepunkteID
-                + '}';
-    }
+  @Override
+  public String toString() {
+    return "AttraktionOeffnungszeit{"
+            + "id=" + id
+            + ", oeffnungszeit=" + oeffnungszeit
+            + '}';
+  }
 }
