63c015925227125cb0b8710b1824ae66eae25d9d
Brian Bonnet <bbonnet@stud.hs-heilbronn.de>
2021-04-14 13:15:32 +0200

    ReisepunktErstellen.vue figma Template übernommen und angepasst

--- src/frontend/src/components/ReisepunktErstellen.vue
+++ src/frontend/src/components/ReisepunktErstellen.vue
@@ -1,5 +1,10 @@
 <template>
+ <div id="HintergrundPanel">
+    <div id="AttraktionErstellen">
 
+    </div>
+
+ </div>
 </template>
 
 <script>
@@ -8,6 +13,480 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
+.AttraktionErstellen {
+  position: absolute;
+  width: 606px;
+  height: 710px;
+  left: 3440px;
+  top: 63px;
+}
+
+
+.HintergrundPanel {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #1B998B;
+  border-radius: 5px;
+}
+
+.Label_Inputfield_Öffnungszeiten {
+  position: absolute;
+  left: 4.95%;
+  right: 5.28%;
+  top: 10.14%;
+  bottom: 80%;
+}
+.Inputfield {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 50%;
+  bottom: 0%;
+  background: #ECECEC;
+}
+
+.Name {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 50%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+
+  color: #ECECEC;
+}
+
+.Label_Inputfield_Name {
+
+  position: absolute;
+  left: 5.12%;
+  right: 5.12%;
+  top: 21.69%;
+  bottom: 68.45%;
+}
+.Inputfield {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 50%;
+  bottom: 0%;
+
+  background: #ECECEC;
+}
+
+.Name {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 50%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+
+  color: #ECECEC;
+}
+.Label_Inputfield_Large {
+
+  position: absolute;
+  left: 5.12%;
+  right: 5.12%;
+  top: 33.24%;
+  bottom: 56.9%;
+}
+
+.Inputfield {
+
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 50%;
+  bottom: -100%;
+
+  background: #ECECEC;
+}
+
+.Beschreibung {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 50%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+
+  color: #ECECEC;
+}
+
+.Buttons {
+  position: absolute;
+  width: 361px;
+  height: 45px;
+  left: 204px;
+  top: 644px;
+}
+
+
+.Button_Grau {
+
+  position: absolute;
+  left: 33.66%;
+  right: 37.95%;
+  top: 90.7%;
+  bottom: 2.96%;
+
+  mix-blend-mode: normal;
+  opacity: 0.9;
+}
+.Btn_Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #4F4F4F;
+  border-radius: 5px;
+}
+.Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ECECEC;
+}
+
+.Button_Orange {
+  position: absolute;
+  left: 64.85%;
+  right: 6.77%;
+  top: 90.7%;
+  bottom: 2.96%;
+
+  mix-blend-mode: normal;
+  opacity: 0.9;
+}
+.Btn_Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #FF9B71;
+  border-radius: 5px;
+}
+
+.Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ECECEC;
+}
+.Auswahl_Reisepunkt {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 92.96%;
+}
+
+.Button {
+  position: absolute;
+  left: 0%;
+  right: 66.67%;
+  top: 0%;
+  bottom: 0%;
+
+  mix-blend-mode: normal;
+}
+
+.Btn_Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #147D72;
+  border-radius: 5px;
+}
+
+.Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ECECEC;
+}
+
+
+.Button {
+  position: absolute;
+  left: 33.33%;
+  right: 33.33%;
+  top: 0%;
+  bottom: 0%;
+
+  mix-blend-mode: normal;
+}
+
+.Btn_Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #147D72;
+  border-radius: 5px;
+}
+
+.Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ECECEC;
+}
+
+
+.Button {
+  position: absolute;
+  left: 66.67%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  mix-blend-mode: normal;
+}
+
+.Btn_Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #32B2A3;
+  border-radius: 5px;
+}
+
+.Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ECECEC;
+}
+.Bilder {
+  position: absolute;
+  width: 535px;
+  height: 232px;
+  left: 30px;
+  top: 382px;
+}
+
+.Bilder {
+  position: absolute;
+  left: 5.12%;
+  right: 6.77%;
+  top: 53.8%;
+  bottom: 41.27%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+
+  color: #ECECEC;
+}
+
+.Button_Small {
+  position: absolute;
+  left: 4.95%;
+  right: 68.15%;
+  top: 59.15%;
+  bottom: 37.04%;
+
+  mix-blend-mode: normal;
+  opacity: 0.9;
+}
+.Btn_Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #4F4F4F;
+  border-radius: 5px;
+}
+
+.Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 17px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ECECEC;
+}
+.Button_Small {
+  position: absolute;
+  left: 32.67%;
+  right: 40.43%;
+  top: 59.15%;
+  bottom: 37.04%;
+
+  mix-blend-mode: normal;
+  opacity: 0.9;
+}
+
+.Btn_Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #4F4F4F;
+  border-radius: 5px;
+}
+.Reisen {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 17px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ECECEC;
+}
+
+.Horizontal Scrollbar {
+  position: absolute;
+  left: 5.12%;
+  right: 8.25%;
+  top: 85.07%;
+  bottom: 13.52%;
+}
+.Background {
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #ECECEC;
+}
+.Bar {
+  position: absolute;
+  left: 0%;
+  right: 9.33%;
+  top: 0%;
+  bottom: 0%;
+
+  background: #4C5756;
+}
 </style>
