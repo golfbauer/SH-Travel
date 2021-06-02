44ffa292b9b5d47e48161aaf4c6159d6e8898797
Brian Bonnet <bbonnet@stud.hs-heilbronn.de>
2021-06-02 11:27:12 +0200

    small fix in Many to Many annotation

--- src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/Reisekatalog.java
+++ src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/Reisekatalog.java
@@ -9,9 +9,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+/**
+ * Reisekatalog Klasse, in dieser werden Reisen gesammelt, gehört einem Nutzer.
+ */
 @Entity
 @Table
 public class Reisekatalog {
@@ -19,27 +23,27 @@ public class Reisekatalog {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String eMail;
-  @ManyToMany(cascade = CascadeType.ALL)
-  @JoinColumn()
+  @ManyToMany(mappedBy = "reisekatalog")
   List<Reise> reise = new ArrayList<>();
 
   /**
-   * Constructor for Reisekatalog with an already existing List of Reisen in it
+   * Constructor for Reisekatalog with an already existing List of Reisen in it.
    * @param id id of this Reisekatalog
    * @param eMail String of the Nutzers eMail
    * @param reise a List with Reisen in it.
    */
-  public Reisekatalog(Long id, String eMail, List<Reise> reise){
+  public Reisekatalog(Long id, String eMail, List<Reise> reise) {
     this.id = id;
     this.eMail = eMail;
     this.reise = reise;
   }
+
   /**
    * Constructor for Reisekatalog without existing Reisen in it.
    * @param id id of this Reisekatalog
    * @param eMail String of the Nutzers eMail
    */
-  public Reisekatalog(Long id, String eMail){
+  public Reisekatalog(Long id, String eMail) {
     this.id = id;
     this.eMail = eMail;
   }
@@ -50,32 +54,35 @@ public class Reisekatalog {
   public Reisekatalog() {
   }
 
-  public String geteMail(){
+  public String geteMail() {
     return this.eMail;
   }
-  public void seteMail(String eMail){
+
+  public void seteMail(String eMail) {
     this.eMail = eMail;
   }
-  public List<Reise> getReise(){
+
+  public List<Reise> getReise() {
     return this.reise;
   }
+
   public void setReise(List<Reise> reise) {
     this.reise = reise;
   }
 
   /**
-   * Adds a Reise to this Reisekatalog
+   * Adds a Reise to this Reisekatalog.
    * @param reise Reise you want to add.
    */
-  public void addReise(Reise reise){
+  public void addReise(Reise reise) {
     this.reise.add(reise);
   }
 
   /**
-   * Removes a Reise from this Reisekatalog
+   * Removes a Reise from this Reisekatalog.
    * @param reise Reise you want to remove.
    */
-  public void removeReise(Reise reise){
+  public void removeReise(Reise reise) {
     this.reise.remove(reise);
   }
 }
