7e8208914300e9276fbba9e336c63fd2f7fdb9b5
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-06-10 19:58:17 +0200

    Added Config File for Reise and Reisekatalog

---
+++ src/main/java/de/hhn/se/labswp/wstgsh/webapi/Configuration/ReisekatalogConfig.java
@@ -0,0 +1,27 @@
+package de.hhn.se.labswp.wstgsh.webapi.Configuration;
+
+import de.hhn.se.labswp.wstgsh.exceptions.ReisepunktNotFoundAdvice;
+import de.hhn.se.labswp.wstgsh.webapi.models.Reise;
+import de.hhn.se.labswp.wstgsh.webapi.models.Reisekatalog;
+import de.hhn.se.labswp.wstgsh.webapi.models.ReisekatalogRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class ReisekatalogConfig {
+  private static final org.slf4j.Logger logger =
+          org.slf4j.LoggerFactory.getLogger(ReisepunktNotFoundAdvice.class);
+
+  @Bean
+  CommandLineRunner commandLineRunnerReise(ReisekatalogRepository repository) {
+    return args -> {
+      List<Reisekatalog> list = new ArrayList<>();
+
+      repository.saveAll(list);
+    };
+  }
+}
