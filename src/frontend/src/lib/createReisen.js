ab26ec84e663c16636c4d51033b48aa6f4a2bbdb
Benjamin Kammer <bkammer@stud.hs-heilbronn.de>
2021-06-10 14:39:13 +0200

    First barebone version without implementation

---
+++ src/frontend/src/lib/createReisen.js
@@ -0,0 +1,20 @@
+/*
+ * Dieses Skript beinhaltet Funktionen die in bezug mit Reisen benötigt werden.
+ */
+
+/*
+ * Diese Funktion überprüft, ob die voraussetzungen vorhanden sind, 
+ * um eine Reise zu erstellen
+ */
+function checkRequirements () {
+  
+  // - Sind mindestens 2 Reisepunkte vorhanden ?
+
+}
+
+/*
+ * Stellt die Funktionen auserhalbs des Skriptes zum aufruf bereit.
+ */
+export {
+    checkRequirements
+}
