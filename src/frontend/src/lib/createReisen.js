eb88c6961b0e0e44c3e8f10ee1c6b36446a0d047
Benjamin Kammer <bkammer@stud.hs-heilbronn.de>
2021-06-10 15:02:19 +0200

    First version of function checkRequirements

--- src/frontend/src/lib/createReisen.js
+++ src/frontend/src/lib/createReisen.js
@@ -2,18 +2,22 @@
  * Dieses Skript beinhaltet Funktionen die in bezug mit Reisen benötigt werden.
  */
 
+import { Store } from "vuex"
+
 /*
  * Diese Funktion überprüft, ob die voraussetzungen vorhanden sind, 
  * um eine Reise zu erstellen
  */
-function checkRequirements () {
-  
-  // - Sind mindestens 2 Reisepunkte vorhanden ?
-
+function checkRequirements (state) {
+  var reisepunkte = Store.getters.getReisepunkte(state) // State notwendig?
+  if (reisepunkte.length() > 1) { // Sind weniger als 2 Reisepunkte vorhanden?
+    return false;
+  }
+  return true
 }
 
 /*
- * Stellt die Funktionen auserhalbs des Skriptes zum aufruf bereit.
+ * Stellt die Funktionen außerhalbs des Skriptes zum Aufruf bereit.
  */
 export {
     checkRequirements
