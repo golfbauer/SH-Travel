ffa1ede6345493f959001f5511e36fb9fd14195a
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-06-13 16:03:13 +0200

    Added Classes for sending Email as well as changing the config file for test porpuses

---
+++ src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/email/EmailValidator.java
@@ -0,0 +1,16 @@
+package de.hhn.se.labswp.wstgsh.webapi.models.email;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+
+@Service
+public class EmailValidator implements Predicate<String> {
+
+
+  @Override
+  public boolean test(String s) {
+    // TODO: Regex to validate email
+    return true;
+  }
+}
