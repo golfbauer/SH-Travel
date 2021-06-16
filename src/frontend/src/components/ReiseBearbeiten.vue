f77cf5ee80be4f46a1ca4a80368f06d638202736
Benjamin Kammer <bkammer@stud.hs-heilbronn.de>
2021-06-16 16:37:04 +0200

    Created new file ReiseBearbeiten.vue

---
+++ src/frontend/src/components/ReiseBearbeiten.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="reise-bearbeiten">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReiseBearbeiten',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
