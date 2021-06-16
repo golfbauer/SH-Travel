93fb60492456b578f88aa75043858da3ea35be25
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-06-16 19:04:31 +0200

    Catching possible errors in Registration and adding a method to resend Tokens

---
+++ src/main/java/de/hhn/se/labswp/wstgsh/webapi/models/registration/RegistrationAntwort.java
@@ -0,0 +1,33 @@
+package de.hhn.se.labswp.wstgsh.webapi.models.registration;
+
+public class RegistrationAntwort {
+
+  private String antwort;
+
+  private String token;
+
+
+  public RegistrationAntwort(String antwort, String token) {
+    this.antwort = antwort;
+    this.token = token;
+  }
+
+  public RegistrationAntwort() {
+  }
+
+  public String getAntwort() {
+    return antwort;
+  }
+
+  public void setAntwort(String antwort) {
+    this.antwort = antwort;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+}
