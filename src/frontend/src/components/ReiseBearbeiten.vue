cd83a5151f780e95d338784953465feff13be9a9
Benjamin Kammer <bkammer@stud.hs-heilbronn.de>
2021-06-17 11:28:58 +0200

    Fist implementation with vuex

--- src/frontend/src/components/ReiseBearbeiten.vue
+++ src/frontend/src/components/ReiseBearbeiten.vue
@@ -1,20 +1,95 @@
 <template>
   <div id="reise-bearbeiten">
+    <b-card no-body v-bind:header="reise.id">
+      <b-list-group flush>
+        <b-list-group-item v-for="reisepunkt in this.reise.reisepunkte" :key="reisepunkt.id">{{reisepunkt}}</b-list-group-item>
+      </b-list-group>
+
+      <b-card-body class="btn-bar">
+        <b-button class="btn-gray" type="reset">Abbrechen</b-button>
+        <b-button class="btn-orange" type="submit">Speichern</b-button>
+      </b-card-body>
+    </b-card>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'ReiseBearbeiten',
   data () {
     return {
+      arrayIndex: 1,
+      reise: {
+        termin: null,
+        name: 'Schloss Besichtigungen',
+        oeffentlich: true,
+        _links: {
+          self: {
+            href: 'http://localhost:8080/reises/2300'
+          },
+          reise: {
+            href: 'http://localhost:8080/reises/2300'
+          },
+          reisekatalog: {
+            href: 'http://localhost:8080/reises/2300/reisekatalog'
+          },
+          reisepunkte: {
+            href: 'http://localhost:8080/reises/2300/reisepunkte'
+          }
+        }
+      }
     }
   },
+  computed: {
+    ...mapGetters(['getReisen'])
+  },
   methods: {
+    ...mapActions(['fetchReisen']),
+    getReise: function () {
+      console.log('getArrayIndex')
+      for (var i = 0; i++; this.getReisen.length) {
+        if (this.getReisen[i].id === this.reiseId) {
+          return this.getReisen[i]
+        }
+      }
+      return undefined
+    }
+  },
+  created () {
+    this.fetchReisen()
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+$orange: orange;
+
+#reise-bearbeiten {
+  position: relative;
+  margin: auto;
+  padding: 0;
+  max-width: 20rem;
+  z-index: 999;
+}
+
+.btn {
+  margin: 0.5rem;
+
+  &-gray {
+    background-color: gray;
+    color: #fff;
+  }
+
+  &-orange {
+    background-color: $orange;
+    color: #fff;
+  }
+
+  &-bar{
+    overflow: hidden;
+    clear: both;
+  }
+}
 
 </style>
