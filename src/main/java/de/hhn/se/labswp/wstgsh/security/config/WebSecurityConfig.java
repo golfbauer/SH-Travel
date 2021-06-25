42276d10e871167683894ec0d62305e986e79d3c
Henri Truetsch <htruet@stud.hs-heilbronn.de>
2021-06-25 18:54:17 +0200

    Added Jwt Configuration

--- src/main/java/de/hhn/se/labswp/wstgsh/security/config/WebSecurityConfig.java
+++ src/main/java/de/hhn/se/labswp/wstgsh/security/config/WebSecurityConfig.java
@@ -37,7 +37,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/", "/register", "/login").permitAll()
+            .antMatchers(
+                    "/", "/register", "/login", "/attraktion/oeffentlich",
+                    "/punkt/oeffentlich", "/reise/oeffentlich", "/reisepunkt/oeffentlich"
+            ).permitAll()
             .anyRequest()
             .authenticated()
             .and()
