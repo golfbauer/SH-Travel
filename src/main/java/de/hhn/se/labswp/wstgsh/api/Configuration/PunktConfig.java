1c5596de74cc5de00e462d41576b93a84da5c18a
Pascal Hauck <geierwali24@gmail.com>
2021-06-30 21:44:06 +0200

    updated the punktconfig

--- src/main/java/de/hhn/se/labswp/wstgsh/api/Configuration/PunktConfig.java
+++ src/main/java/de/hhn/se/labswp/wstgsh/api/Configuration/PunktConfig.java
@@ -1,10 +1,14 @@
 package de.hhn.se.labswp.wstgsh.api.Configuration;
 
+import de.hhn.se.labswp.wstgsh.api.models.Punkt;
 import de.hhn.se.labswp.wstgsh.exceptions.ReisepunktNotFoundAdvice;
 import de.hhn.se.labswp.wstgsh.api.models.PunktRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Configuration
@@ -25,43 +29,43 @@ public class PunktConfig {
       /*List<Punkt> list = new ArrayList<>();
       Punkt omaBeate = new Punkt(
               10.692964196205141f, 53.867987608631005f,
-              "Henri@gmx.de", "Oma Beate"
+              "Oma Beate", true
       );
       Punkt opaKarlHeins = new Punkt(
               9.990327358245851f, 54.06959447701732f,
-              "Henri@gmx.de", "Omp Karl Heins"
+               "Omp Karl Heins", true
       );
       Punkt zwischenStop = new Punkt(
               9.605441093444826f, 54.420602956508525f,
-              "Brian@gmx.de", "Halbe Strecke Rast"
+               "Halbe Strecke Rast", true
       );
       Punkt tinglevUmstieg = new Punkt(
               9.258213043212892f, 54.93557467072232f,
-              "Brian@gmx.de", "Umstieg an Tinglev"
+               "Umstieg an Tinglev", true
       );
       Punkt padborgAusstieg = new Punkt(
               9.605441093444826f, 54.420602956508525f,
-              "Brian@gmx.de", "Ausstieg an Padborg"
+               "Ausstieg an Padborg", true
       );
       Punkt lieblingsstrand = new Punkt(
               8.62804412841797f, 54.89788457498233f,
-              "Ben@gmx.de", "Mein Lieblingsstrand"
+              "Mein Lieblingsstrand", true
       );
       Punkt grenzeNachDaenemark = new Punkt(
               9.330139160156252f, 54.80068486732236f,
-              "Ben@gmx.de", "Grenze nach Dänemark"
+              "Grenze nach Dänemark", true
       );
       Punkt stelleZumFischen = new Punkt(
               10.496642589569094f, 54.387630018013894f,
-              "Dennis@gmx.de", "Die perfekte Stelle zum Fischen"
+               "Die perfekte Stelle zum Fischen", true
       );
       Punkt schlachtkreuzer = new Punkt(
               10.208702087402346f, 54.395351059646764f,
-              "Dennis@gmx.de", "Ein beendruckender Schlachtkreuzer"
+               "Ein beendruckender Schlachtkreuzer", true
       );
       Punkt meinEigenesBoot = new Punkt(
               10.021140575408937f, 54.6625385923639f,
-              "Pascal@gmx.de", "Mein Eigenes Boot"
+              "Mein Eigenes Boot", true
       );
       list.add(omaBeate);
       list.add(opaKarlHeins);
