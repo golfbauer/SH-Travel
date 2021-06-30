0d81f55f05c130e58edcdf5618b5b67696b098ab
Dennis Schies <dschies@stud.hs-heilbronn.de>
2021-06-30 16:46:16 +0200

    Fixed issues with not showing journeys

--- src/frontend/src/components/SidebarMenuItem.vue
+++ src/frontend/src/components/SidebarMenuItem.vue
@@ -2,16 +2,17 @@
   <div>
     <div :id="item.component">
       <div v-if="item.component==='parent'">
-        <b-nav-item @click="toggle()" class="bor-bot"> <!--:href='item.href'-->
+        <b-nav-item @click="toggle()" class="bor-bot">
           <div class="dropdown-toggle">
             {{ item.name }}
           </div>
         </b-nav-item>
         <div v-if="showChild">
-          <sidebar-menu-item v-on:click.native="handleClick(subitem)" v-for="(subitem, index) in this.item.content" :key="index" :item="subitem"/>
+          <sidebar-menu-item v-on:click.native="handleClick(subitem.content)" v-for="(subitem, index) in childItems"
+                             :key="index" :item="subitem"/>
         </div>
       </div>
-      <div v-if="item.component !== 'parent'">
+      <div v-if="item.component === 'child'">
         {{ item.name }}
       </div>
     </div>
@@ -26,7 +27,8 @@ export default {
   data () {
     return {
       showChild: true,
-      chosenChild: undefined
+      chosenChild: undefined,
+      childItems: []
     }
   },
   props: {
@@ -34,12 +36,26 @@ export default {
       type: Object,
       required: true,
       // optional properties
+      name: '',
       component: undefined,
       category: undefined,
       content: undefined
     }
   },
   methods: {
+    init () {
+      const item = this.item
+      if (item.component === 'parent' && item.content !== undefined) {
+        for (let i = 0; i < item.content.length; i++) {
+          const child = {
+            name: item.content[i].name,
+            component: 'child',
+            content: item.content[i]
+          }
+          this.childItems.push(child)
+        }
+      }
+    },
     handleClick (subitem) {
       switch (this.item.category) {
         case 'reisen':
@@ -64,7 +80,10 @@ export default {
     }
   },
   created () {
+    console.log('Component', this.item.component)
     console.log('Content', this.item.content)
+
+    this.init()
   }
 }
 </script>
@@ -73,19 +92,17 @@ export default {
 
 @import "./src/assets/style.scss";
 
-.bor-bot{
-  border-bottom: #000 solid 1px;
+.bor-bot {
+  border-bottom: solid 1px #1A776A;
 }
 
 #item {
   background-color: $primary-color;
 
-  a {
-    color: #fff;
+  color: #fff;
 
-    &:hover {
-      color: #000;
-    }
+  &:hover {
+    color: #000;
   }
 }
 
@@ -116,12 +133,19 @@ export default {
 #child {
   background-color: $primary-color-2;
 
-  a {
-    color: #fff;
+  position: relative;
+  box-sizing: border-box;
 
-    &:hover {
-      color: #000;
-    }
+  height: 2.5em;
+  border-bottom: 0.1em solid #1A776A;
+
+  text-align: center;
+  vertical-align: middle;
+  line-height: 2.5em;
+
+  &:hover {
+    background-color: #39f68e;
+    color: #FFFFFF;
   }
 }
 </style>
